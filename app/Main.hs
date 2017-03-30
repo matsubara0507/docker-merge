15820593719f56ed1bd510473e4cdc2dc2b0b78f
松原信忠 <t12307043@gunma-u.ac.jp>
2017-03-30 18:44:17 +0900

    Updated Main.hs
    show only Dockerfile's inspection

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
+import Data.Either (lefts, rights)
 import Options.Applicative
 import Language.Dockerfile
+import Language.Dockerfile.Syntax
 
 data Args = Args [FilePath] deriving (Show)
 
@@ -11,8 +13,19 @@ argsParser = Args <$> many (argument filepath (metavar "TARGET..."))
 filepath :: ReadM FilePath
 filepath = str
 
+printDockerfile :: Dockerfile -> String
+printDockerfile dockerfile = unlines $ (filename `mappend` ":") : instructions
+  where
+    filename = sourcename $ head dockerfile
+    instructions = fmap (show .instruction) dockerfile
+
 main :: IO ()
 main = do
   Args filepaths <- execParser (info (argsParser <**> helper) idm)
   dockerfiles <- mapM parseFile filepaths
-  print dockerfiles
+
+  putStrLn "== can read =="
+  putStrLn . unlines . fmap printDockerfile $ rights dockerfiles
+
+  putStrLn "== can't read =="
+  print $ lefts dockerfiles
