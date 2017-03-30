385da85e43707d4ffc05be0e666633dc4a526fed
松原信忠 <t12307043@gunma-u.ac.jp>
2017-03-30 18:15:46 +0900

    Updated Main.hs
    parsing options of cli

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import Options.Applicative
+
+data Args = Args [FilePath] deriving (Show)
+
+argsParser :: Parser Args
+argsParser = Args <$> many (argument filepath (metavar "TARGET..."))
+
+filepath :: ReadM FilePath
+filepath = str
+
 main :: IO ()
-main = putStrLn "hello, world!"
+main = do
+  Args fps <- execParser (info (argsParser <**> helper) idm)
+  putStrLn $ unlines fps
