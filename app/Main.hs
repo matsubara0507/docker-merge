e6def342b0c4976fe9baafd49db142353514b653
松原信忠 <t12307043@gunma-u.ac.jp>
2017-03-31 14:10:39 +0900

    Updated Main.hs
    make function that merge Dockerfile

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Either (lefts, rights)
+import Data.Maybe (fromMaybe)
 import Options.Applicative
 import Language.Dockerfile
 import Language.Dockerfile.Syntax
@@ -13,11 +14,27 @@ argsParser = Args <$> many (argument filepath (metavar "TARGET..."))
 filepath :: ReadM FilePath
 filepath = str
 
+toInstructions :: Dockerfile -> [Instruction]
+toInstructions = fmap instruction
+
+removeEOL :: [Instruction] -> [Instruction]
+removeEOL = filter (/= EOL)
+
+getFileName :: Dockerfile -> Maybe Filename
+getFileName [] = Nothing
+getFileName df = Just . sourcename . head $ df
+
+mergeDockerfiles :: [Instruction] -> [Instruction] -> [Instruction]
+mergeDockerfiles [] df = df
+mergeDockerfiles xs@(x:xs') ys@(y:ys')
+  | x == y = x : mergeDockerfiles xs' ys'
+  | otherwise = xs `mappend` ys
+
 printDockerfile :: Dockerfile -> String
 printDockerfile dockerfile = unlines $ (filename `mappend` ":") : instructions
   where
-    filename = sourcename $ head dockerfile
-    instructions = fmap (show .instruction) dockerfile
+    filename = fromMaybe "" . getFileName $ dockerfile
+    instructions = fmap show . removeEOL . toInstructions $ dockerfile
 
 main :: IO ()
 main = do
@@ -26,6 +43,9 @@ main = do
 
   putStrLn "== can read =="
   putStrLn . unlines . fmap printDockerfile $ rights dockerfiles
+
+  putStrLn "== merge =="
+  putStrLn . unlines . fmap show . foldl mergeDockerfiles [] . fmap (removeEOL . toInstructions) $ rights dockerfiles
 
   putStrLn "== can't read =="
   print $ lefts dockerfiles
